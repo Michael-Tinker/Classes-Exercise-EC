4796a33f0f949ad2dbf5c1a2b7bc8c451882df0e
Michael-Tinker <michaelt19@protonmail.com>
2022-03-01 13:33:38 -0600

    Initial Commit

---
+++ ServiceQuoteClass.py
@@ -0,0 +1,14 @@
+#Class Name: ServiceQuote
+
+#Data Attributes:
+#__parts_charges
+#__labor_charges
+
+#Methods:
+#__init__(pcharge, lcharge)
+#set_parts_charges(pcharge)
+#set_labor_charges(lcharge)
+#get_parts_charges()
+#get_labor_charges()
+#get_sales_tax()
+#get_total_charges()
